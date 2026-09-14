8eb41eb307993739ea39276b28232c462fe5e91d
rahulr21954-create <rahulr21954@gmail.com>
2026-09-15 00:38:47 +0530

    Update socket connection URL to use VITE_API_URL

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(`http://localhost:${process.env.PORT}`, {
+const socket = io(`http://localhost:${import.meta.env.VITE_API_URL}`, {
   autoConnect: true,
 });
 
