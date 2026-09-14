306aada1f6c56f5877134746dbf99ba54c21abce
rahulr21954-create <rahulr21954@gmail.com>
2026-09-15 00:37:38 +0530

    Update API URL to use VITE_API_URL environment variable

--- frontend/src/services/hospitalService.js
+++ frontend/src/services/hospitalService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = `http://localhost:${process.env.PORT}/api/hospitals`;
+const API = `http://localhost:${import.meta.env.VITE_API_URL}/api/hospitals`;
 
 export const getNearbyHospitals = async (lat, lon) => {
   const res = await axios.get(`${API}/nearby`, {
