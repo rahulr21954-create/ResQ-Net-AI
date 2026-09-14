f409c99865d5a2dc4c2a04f93c628e5b81f8328c
rahulr21954-create <rahulr21954@gmail.com>
2026-09-15 00:55:04 +0530

    Remove localhost from emergency API URL

--- frontend/src/services/emergencyService.js
+++ frontend/src/services/emergencyService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = `http://localhost:${import.meta.env.VITE_API_URL}/api/emergency`;
+const API = `${import.meta.env.VITE_API_URL}/api/emergency`;
 
 export const createEmergency = async (data, token) => {
   console.log("Sending request to backend...");
