fb2cd0680b1c20725386ee1de8e592ad3584e660
rahulr21954-create <rahulr21954@gmail.com>
2026-09-15 00:37:12 +0530

    Update historyService.js

--- frontend/src/services/historyService.js
+++ frontend/src/services/historyService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = `http://localhost:${process.env.PORT}/api/emergency`;
+const API = `http://localhost:${import.meta.env.VITE_API_URL}/api/emergency`;
 
 export const getEmergencyHistory = async () => {
   const token = localStorage.getItem("token");
