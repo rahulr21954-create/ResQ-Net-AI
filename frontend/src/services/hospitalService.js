f60ab86566dfc3a0f136546293bd387efa2a5243
rahulr21954-create <rahulr21954@gmail.com>
2026-09-15 00:55:51 +0530

    Remove localhost from hospital API URL

--- frontend/src/services/hospitalService.js
+++ frontend/src/services/hospitalService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = `http://localhost:${import.meta.env.VITE_API_URL}/api/hospitals`;
+const API = `${import.meta.env.VITE_API_URL}/api/hospitals`;
 
 export const getNearbyHospitals = async (lat, lon) => {
   const res = await axios.get(`${API}/nearby`, {
