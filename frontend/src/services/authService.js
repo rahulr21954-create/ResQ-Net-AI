05df56551336fce58b020e7a0c2c9ce1927dbfba
rahulr21954-create <rahulr21954@gmail.com>
2026-09-15 00:54:42 +0530

    Remove localhost from API URL in authService

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = `http://localhost:${import.meta.env.VITE_API_URL}/api/auth`;
+const API = `${import.meta.env.VITE_API_URL}/api/auth`;
 
 export const signupUser = async (data) => {
   const res = await axios.post(`${API}/signup`, data);
