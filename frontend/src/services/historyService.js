00705900c376c5d209717cf66cd738e5e394bee6
rahulr21954-create <rahulr21954@gmail.com>
2026-09-15 00:55:23 +0530

    Remove localhost from API URL in historyService

--- frontend/src/services/historyService.js
+++ frontend/src/services/historyService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = `http://localhost:${import.meta.env.VITE_API_URL}/api/emergency`;
+const API = `${import.meta.env.VITE_API_URL}/api/emergency`;
 
 export const getEmergencyHistory = async () => {
   const token = localStorage.getItem("token");
