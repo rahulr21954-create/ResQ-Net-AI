a092b2326c065e0004449d154bdd4f9d44f128b2
rahulr21954-create <rahulr21954@gmail.com>
2026-09-14 22:59:03 +0530

    Fix import path for User model in authController

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,5 @@
 import generateToken from "../utils/generateTokens.js";
-import User from "../models/user.js";
+import User from "../models/User.js";
 
 export const signup=async (req,res)=>{
     try {
