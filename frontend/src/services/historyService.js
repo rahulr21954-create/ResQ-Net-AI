e1e0b27ecd1dc32a78923945b88c118dcdd0c8cd
rahulr21954-create <rahulr21954@gmail.com>
2026-09-15 00:14:11 +0530

    Use environment variable for API port

--- frontend/src/services/historyService.js
+++ frontend/src/services/historyService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8000/api/emergency";
+const API = `http://localhost:${process.env.PORT}/api/emergency`;
 
 export const getEmergencyHistory = async () => {
   const token = localStorage.getItem("token");
