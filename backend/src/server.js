1522636ad6b731924fee5b441063b78876b19eb5
rahulr21954-create <rahulr21954@gmail.com>
2026-09-15 01:20:49 +0530

    Update CORS origin for Socket.IO server

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const server=createServer(app);
 
 export const io= new Server(server,{
     cors:{
-        origin: ["http://localhost:5173","https://resq-net-ai.onrender.com"],
+        origin: ["http://localhost:5173","https://resq-net-ai-eta.vercel.app"],
         methods: ["GET", "POST"],
         credentials: true,
     }
