9b62e2f4a3dfe2437cac0da5d776261707d7007e
rahulr21954-create <rahulr21954@gmail.com>
2026-09-15 00:36:08 +0530

    Update API URL to use VITE_API_URL environment variable

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = `http://localhost:${process.env.PORT}/api/auth`;
+const API = `http://localhost:${import.meta.env.VITE_API_URL}/api/auth`;
 
 export const signupUser = async (data) => {
   const res = await axios.post(`${API}/signup`, data);
